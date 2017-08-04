e6b612656f04e7400bb49a29ce42cf466c5534a3
Akshay Mathur <akshaym@sahusoft.com>
2017-08-04 19:48:28 +0530

    added react bootstrap and carousel

--- src/App.js
+++ src/App.js
@@ -2,51 +2,118 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BarChart,Bar, CartesianGrid, Tooltip, XAxis, YAxis, Legend} from 'recharts';
-
+import { Carousel } from 'react-bootstrap';
 // import Question from './questions';
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      questionList: [
-        {
-          questionText: 'Who will die next in game of thrones?',
-          choices: ['Tyrion','Arya','Littlefinger']
+      questions : {
+        'who will die next': {
+          'a': {
+            'name': 'Tyrion',
+            'votes': 3
+          },
+          'b': {
+            'name': 'Arya',
+            'votes': 5
+          },
+          'c': {
+            'name': 'LittleFinger',
+            'votes': 8
+          },
         },
-        {
-          questionText: 'Who will live next in game of thrones?',
-          choices: ['Tillu','Millu','Killu']
+        'who will marry next': {
+          'a': {
+            'name': 'Tyrion',
+            'votes': 4
+          },
+          'b': {
+            'name': 'Arya',
+            'votes': 7
+          },
+          'c': {
+            'name': 'LittleFinger',
+            'votes': 50
+          }
         },
-        {
-          questionText: 'Who will kill next in game of thrones?',
-          choices: ['Talla','Malla','Kalla']
+        'who will kill next': {
+          'a': {
+            'name': 'Tyrion',
+            'votes': 3
+          },
+          'b': {
+            'name': 'Arya',
+            'votes': 9
+          },
+          'c': {
+            'name': 'LittleFinger',
+            'votes': 21
+          },
         }
-      ],
-      selectedIndex: 0,
+      },
       isGraphVisible: false,
-      data : [
-        {name: 'Page A', pv: 2400, amt: 2400},
-        {name: 'Page B', pv: 1398, amt: 2210},
-        {name: 'Page C', pv: 9800, amt: 2290},
-      ]
+      activeVotes : [
+        {name: 'Page A',votes: 2400},
+        {name: 'Page B',votes: 2210},
+        {name: 'Page C',votes: 2290},
+      ],
+      index: 0,
+      direction: null
     }
   }
+    handleSelect(selectedIndex, e) {
+      this.setState({
+        index: selectedIndex,
+        direction: e.direction,
+        isGraphVisible: false
+      });
+    }
+    handleGraphClick(val, question) {
+      const { questions } = this.state;
+      let activeVotes = [];
+      var obj;
+      this.setState({isGraphVisible: val});
+      Object.keys(questions[question]).forEach((choice, index) => {
+        obj = {
+          name: questions[question][choice].name,
+          votes: questions[question][choice].votes
+        }
+        console.log(obj);
+        activeVotes.push(obj)
+      })
+      console.log(activeVotes);
+      this.setState({
+        activeVotes:activeVotes
+      })
+    }
   render() {
+    const { questions } = this.state
     return (
       <Container>
-        <Header>React Native Banglore Meetup</Header>
-        <Question>
-          <QuestionText>{this.state.questionList[this.state.selectedIndex].questionText}</QuestionText>
+        <Header>React Native Bangalore Meetup</Header>
+        <Carousel activeIndex={this.state.index} direction={this.state.direction} onSelect={this.handleSelect.bind(this)}>
           {
-            this.state.questionList[this.state.selectedIndex].choices.map((choice, index) => 
-              <Choice>{index+1} {choice}</Choice>
-            )
+            Object.keys(questions).map((question, index) => (
+              <Carousel.Item>
+                <Question>
+                  <QuestionText>{question}</QuestionText>
+                  {
+                    Object.keys(questions[question]).map((choice, index) => 
+                      <Choice>
+                        <span>{choice}</span>
+                        <span>{questions[question][choice].name}</span>
+                      </Choice>
+                    )
+                  }
+                </Question>
+                    <Graph active={(val) => this.handleGraphClick(val, question)}>
+                       {this.state.isGraphVisible && <ShowGraph data={this.state.activeVotes}/>}
+                    </Graph>
+              </Carousel.Item>
+            ))
           }
-        </Question>
-        <Counter increment={() => this.setState({selectedIndex: this.state.selectedIndex + 1, isGraphVisible: false})}>{this.state.selectedIndex}</Counter>
-        <Graph active={(val) => this.setState({isGraphVisible: val})}>
-          {this.state.isGraphVisible && <ShowGraph data={this.state.data}/>}
-        </Graph>
+        </Carousel>
       </Container>
     );
   }
@@ -64,10 +131,11 @@ const Header = (props) => {
     </div>
   )
 }
+
 const Question = (props) => {
-    return(
-      <div>{props.children}</div>
-    );
+  return(
+    <div>{props.children}</div>
+  );
 }
 const QuestionText = (props) => {
     return(
@@ -79,6 +147,7 @@ const Choice = (props) => {
       <div>{props.children}</div>
     )
 }
+
 const Counter = (props) => {
   return(
     <div>
@@ -107,11 +176,56 @@ const ShowGraph = (props) => {
         <CartesianGrid strokeDasharray="3 3" />
         <Tooltip />
         <Legend />
-        <Bar dataKey="pv" fill="#8884d8" />
-        <Bar dataKey="uv" fill="#82ca9d" />
+        <Bar dataKey="votes" fill="#8884d8" />
       </BarChart>
     </div>
   )
 }
+class ControlledCarousel extends Component {
+  constructor(props) {
+    super(props);
+    this.state =  {
+      index: 0,
+      direction: null
+    };
+  }
+
+  handleSelect(selectedIndex, e) {
+    alert('selected=' + selectedIndex + ', direction=' + e.direction);
+    this.setState({
+      index: selectedIndex,
+      direction: e.direction
+    });
+  }
+
+  render() {
+    return (
+      <Carousel activeIndex={this.state.index} direction={this.state.direction} onSelect={(selectedIndex, e) => this.handleSelect(selectedIndex, e)}>
+        <Carousel.Item>
+
+          <div>here the question will go</div>
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div>here the question will go</div>
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div>here the question will go</div>
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    );
+  }
+};
 
 export default App;
